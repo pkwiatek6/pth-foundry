720cf9f3562505b8389d87c4d1fda5a54b2f0665
pkwiatek6 <pkwiatek6@gmail.com>
2024-09-17 21:43:48 -0400

    added template for charms in pth.mjs

---
+++ pth/module/pth.mjs
@@ -0,0 +1,13 @@
+// Define the CharmDocument class
+class CharmDocument extends Item {
+    // Your custom document logic here
+  }
+  CONFIG.Item.documentClass = CharmDocument;
+  
+  // Define the CharmSheet class
+  class CharmSheet extends ItemSheet {
+    // Your custom sheet logic here
+  }
+  Items.unregisterSheet("core", ItemSheet);
+  Items.registerSheet("pth", CharmSheet, { types: ["charm"], makeDefault: true });
+  
