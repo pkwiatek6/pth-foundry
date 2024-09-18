c478e2e89b4f86a0851d668fbdec4a6aaf3feb6f
pkwiatek6 <pkwiatek6@gmail.com>
2024-09-17 21:46:31 -0400

    applied formating

--- pth/module/pth.mjs
+++ pth/module/pth.mjs
@@ -1,13 +1,12 @@
 // Define the CharmDocument class
 class CharmDocument extends Item {
     // Your custom document logic here
-  }
-  CONFIG.Item.documentClass = CharmDocument;
-  
-  // Define the CharmSheet class
-  class CharmSheet extends ItemSheet {
+}
+CONFIG.Item.documentClass = CharmDocument;
+
+// Define the CharmSheet class
+class CharmSheet extends ItemSheet {
     // Your custom sheet logic here
-  }
-  Items.unregisterSheet("core", ItemSheet);
-  Items.registerSheet("pth", CharmSheet, { types: ["charm"], makeDefault: true });
-  
+}
+Items.unregisterSheet("core", ItemSheet);
+Items.registerSheet("pth", CharmSheet, { types: ["charm"], makeDefault: true });
